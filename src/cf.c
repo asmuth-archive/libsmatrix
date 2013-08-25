0ddf1a703cfaa303f4ec2dbc19efce0865719f90
Paul Asmuth <paul@paulasmuth.com>
2013-08-25 21:52:19 +0200

    cf_add_session stub

---
+++ src/cf.c
@@ -0,0 +1,12 @@
+// This file is part of the "recommendify" project
+//   (c) 2011-2013 Paul Asmuth <paul@paulasmuth.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include "cf.h"
+
+void cf_add_session(smatrix_t* smatrix, uint32_t* session, size_t size) {
+
+}
--- src/recommendify.c
+++ src/recommendify.c
@@ -18,16 +18,53 @@ smatrix_t* db;
 int main(int argc, char **argv) {
   db = smatrix_init();
 
-  printf("INIT %p, %i\n", db, db->size);
-  smatrix_lookup(db, 53415246, 22361353, 1);
+  // FNORD
+  printf("> importing /tmp/reco_in.csv...\n");
+  int sess_count;
+  size_t buf_len;
+  char *buf, *cur;
+  FILE *f = fopen("/tmp/reco_in.csv", "r");
+
+  while (1) {
+    if (getline(&buf, &buf_len, f) == -1)
+     break;
+
+    uint32_t sess[500];
+    int sess_len = 0;
+    char *last = buf;
+
+    for (cur = buf; *cur; cur++) {
+      if (*cur == ',' || *cur == '\n') {
+        *cur = 0;
+        if (sess_len < (sizeof(sess) / sizeof(uint32_t)) - 1) {
+          sess[sess_len] = atoi(last);
+          sess_len++;
+        }
+        last = cur + 1;
+      }
+    }
+
+    sess_count++;
+    cf_add_session(sess, sess_len);
+  }
+
+  fclose(f);
+  printf("   * imported %i sessions\n", sess_count);
+  // EOFNORD
+
+  smatrix_vec_t* val = smatrix_lookup(db, 53415246, 22361353, 1);
+  val->value += 1;
+
   smatrix_lookup(db, 53415246, 22361353, 1);
   smatrix_lookup(db, 53415249, 22361353, 1);
   smatrix_lookup(db, 53415249, 22361353, 1);
   smatrix_lookup(db, 53415249, 22361359, 1);
   smatrix_lookup(db, 53415249, 22361359, 1);
-  printf("...\n");
 
   print_version();
+
+  smatrix_free(db);
+
   return 0;
 }
 
