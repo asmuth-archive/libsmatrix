ea72dd7959ae7eec5bd30280f61d88fed3a19c77
Paul Asmuth <paul@paulasmuth.com>
2013-11-30 21:38:09 +0100

    SparseMatrix java interface WIP

--- src/com/paulasmuth/libsmatrix/SparseMatrix.java
+++ src/com/paulasmuth/libsmatrix/SparseMatrix.java
@@ -13,49 +13,56 @@ import java.io.File;
  * A libsmatrix sparse matrix
  */
 public class SparseMatrix {
+  private static String library_path = "libsmatrix.so";
   private long ptr;
 
   /**
-   * Create a new sparse matrix.
-   *
-   * If file_path is null the matrix will be stored completely in main memory.
-   *
-   * If file_path is not null, the matrix will be persisted to disk and only a
-   * cache of the data is kept in main memory (the cache size can be adjusted
-   * with setCacheSize). If the file pointed to by filename exists it will be
-   * opened, otherwise a new file will be created.
+   * Create a new sparse matrix that will be stored in main memory only.
    *
    * @param file_path path to the file or null
    * @return a new SparseMatrix
    */
-  public SparseMatrix(String file_path) {
-    System.loadLibrary("libsmatrix");
-
-    if (file_path == null) {
-      init_memonly();
-    } else {
-      init_file(file_path);
-    }
+  public SparseMatrix() {
+    SparseMatrix.loadLibrary();
+    init(null);
   }
 
   /**
-   * Create a new sparse matrix and load the native shared library from a
-   * specified source path.
+   * Create a new sparse matrix that will be persisted to disk. A cache of the
+   * data is still kept in main memory (the cache size can be adjusted by calling
+   * setCacheSize on the instance). 
    *
-   * If file_path is null the matrix will be stored completely in main memory.
+   * If the file pointed to by filename exists it will be opened, otherwise a new
+   * file will be created.
    *
-   * If file_path is not null, the matrix will be persisted to disk and only a
-   * cache of the data is kept in main memory (the cache size can be adjusted
-   * with setCacheSize). If the file pointed to by filename exists it will be
-   * opened, otherwise a new file will be created.
-   *
-   * library_path is the path to the libsmatrix.so file.
-   *
-   * @param file_path path to the file or null
-   * @param library_path path to the libsmatrix.so file
+   * @param file_path path to the file
    * @return a new SparseMatrix
    */
-  public SparseMatrix(String file_path, String library_path) {
+  public SparseMatrix(String file_path) {
+    SparseMatrix.loadLibrary();
+    init(file_path);
+  }
+
+  /**
+   * Explcitly set the path to the native shared object (libsmatrix.so).
+   *
+   * @param the path to the native shared object
+   */
+  public static void setLibraryPath(String path) {
+    library_path = path;
+    loadLibrary();
+  }
+
+  /**
+   * Close this matrix. Calling any other method on the instance after it was
+   * closed will throw an exception.
+   */
+  public native void close();
+
+  /**
+   * Load the native shared object (libsmatrix.so)
+   */
+  private static void loadLibrary() {
     File libfile = new File(library_path);
 
     if (libfile.exists()) {
@@ -63,21 +70,11 @@ public class SparseMatrix {
     } else {
       System.loadLibrary("libsmatrix");
     }
-
-    if (file_path == null) {
-      init_memonly();
-    } else {
-      init_file(file_path);
-    }
   }
 
   /**
-   * Closes this smatrix. Calling any other method on an instance after it
-   * was closed will throw an exception.
+   * Initialize a new native matrix
    */
-  public native void close();
-
-  private native void init_memonly();
-  private native void init_file(String file_path);
+  private native void init(String file_path);
 
 }
