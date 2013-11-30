a9296207230a5ce8024c3e69ad0e30077d996c5d
Paul Asmuth <paul@paulasmuth.com>
2013-11-30 20:13:32 +0100

    hello world jni, hardcode java include path for osx

---
+++ src/smatrix_jni.c
@@ -0,0 +1,13 @@
+/**
+ * This file is part of the "libsmatrix" project
+ *   (c) 2011-2013 Paul Asmuth <paul@paulasmuth.com>
+ *
+ * Licensed under the MIT License (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of
+ * the License at: http://opensource.org/licenses/MIT
+ */
+#include "smatrix_jni.h"
+
+JNIEXPORT void JNICALL Java_com_paulasmuth_libsmatrix_SparseMatrix_test (JNIEnv* env, jobject self) {
+  printf("hallo jni! :)\n");
+}
