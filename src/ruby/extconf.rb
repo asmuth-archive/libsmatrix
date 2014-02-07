e3d45e0942b8aae58dc59bc57c66bdace683e20b
Paul Asmuth <paul@paulasmuth.com>
2014-02-07 18:02:31 +0100

    compile against static ruby

--- src/ruby/extconf.rb
+++ src/ruby/extconf.rb
@@ -4,6 +4,10 @@ mkmf_includes = <<EOF
 RUBY_INCLUDE      = #{RbConfig::CONFIG["rubyhdrdir"]}
 RUBY_INCLUDE_ARCH = #{RbConfig::CONFIG["rubyhdrdir"]}/#{RbConfig::CONFIG["arch"]}
 RUBY_LIB          = #{RbConfig::CONFIG["libdir"]}
+RUBY_SO_NAME      = #{RbConfig::CONFIG['RUBY_SO_NAME']}
+LIBRUBYARG_SHARED = #{$LIBRUBYARG_SHARED}
+LIBRUBYARG_STATIC = #{$LIBRUBYARG_STATIC}
+LIBRUBYARG        = #{$LIBRUBYARG}
 EOF
 
 File.open(::File.expand_path("../Makefile.in", __FILE__), "w+") do |f|
